79101a9000cc962180ea912aa5ada287f565429c
Lucas Jin <kblazer20@gmail.com>
2025-06-04 14:18:10 -0400

    Congrats page

---
+++ frontend/app/congrats/page.js
@@ -0,0 +1,7 @@
+export default function Congrats() {
+  return (
+    <div className="flex justify-center items-center">
+        <img src="/congrats.gif" alt="Congratulations GIF." className="w-full h-fit"></img>
+    </div>
+  );
+}
